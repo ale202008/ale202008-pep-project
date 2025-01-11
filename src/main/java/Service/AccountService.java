0bf284297458f622aa035f4ad2fa29c8d532e557
Andrew Le <a.asterisk.le@gmail.com>
2025-01-11 03:25:06 +0000

    Setup AccountService.java and initliazed register method for User registration.

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,24 @@
 package Service;
 
+// Importing Model Account
+import Model.Account;
+// Import Account DAO
+import DAO.AccountDAO;
+
+
 public class AccountService {
-    
+    // Initialize accountDAO
+    private AccountDAO accountDAO;
+
+    // Initialize no-args constructor
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    // Use AccountDAO to register a new user
+    // @param username, password
+    // @return Account, null if account exists already
+    public Account register(String name, String password){
+        return null;
+    }
 }
