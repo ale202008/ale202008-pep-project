695b231983c0cea4e3ca0658e1f3fcb0e626f928
Andrew Le <a.asterisk.le@gmail.com>
2025-01-11 03:32:47 +0000

    Cleaned up code in AccountService.java. Added getAllAccounts method Added conditionals for user registration failure.

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -8,7 +8,26 @@ import Model.Account;
 import Util.ConnectionUtil;
 // sql import
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountDAO {
     
+    // Get all Accounts
+    public List<Account> getAllAccounts(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Account> accounts = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Account";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account account = new Account(rs.getString("username"), rs.getString("password"));
+                accounts.add(account);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return accounts;
+    }
 }
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -5,6 +5,7 @@ import Model.Account;
 // Import Account DAO
 import DAO.AccountDAO;
 
+import java.util.List;
 
 public class AccountService {
     // Initialize accountDAO
@@ -15,10 +16,19 @@ public class AccountService {
         accountDAO = new AccountDAO();
     }
 
+    // Get all Accounts via AccountDAO
+    public List<Account> getAllAccounts(){
+        return accountDAO.getAllAccounts();
+    }
+
     // Use AccountDAO to register a new user
     // @param username, password
     // @return Account, null if account exists already
-    public Account register(String name, String password){
+    public Account register(Account account){
+        if (account.getPassword().length() <= 4 || account.getUsername() == "" || this.getAllAccounts().contains(account)){
+            return null;
+        }
+
         return null;
     }
 }
