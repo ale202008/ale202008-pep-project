25ab33b611240dd33dc8779575478d997630f81b
Andrew Le <a.asterisk.le@gmail.com>
2025-01-11 17:57:07 +0000

    Beginning implementation for Message test cases. Implemented GetAllMessages method

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,37 @@
 package DAO;
 
+// Account Model import
+import Model.Message;
+// ConnectionUtil import for database connection
+import Util.ConnectionUtil;
+// sql import
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageDAO {
+
+    // Get all Messages
+    public List<Message> getAllMessages(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM Account";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Message message = new Message(
+                    rs.getInt("posted_by"),
+                    rs.getString("message_text"),
+                    rs.getLong("time_posted_epoch")
+                );
+                messages.add(message);
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
     
 }
